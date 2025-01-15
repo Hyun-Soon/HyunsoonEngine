5801f186456fc0faab049a6a2d0fc0d70e86d9d2
Hyunsoon <66724166+Hyun-Soon@users.noreply.github.com>
2025-01-15 20:18:09 +0900

    feat: DamageUtils

---
+++ HyunsoonEngine_CommonSources/DamageUtils.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <random>
+#include <string>
+
+class DamageUtils
+{
+public:
+	static float CalculateDamage(float baseDamage)
+	{
+		std::random_device				   rd;		  // 시드 생성기
+		std::mt19937					   gen(rd()); // 난수 생성기
+		int								   range = pow(9, (std::to_string(baseDamage).size()));
+		std::uniform_int_distribution<int> dist(baseDamage - range, baseDamage + range); // 실수 범위 설정
+
+		return dist(gen); // 난수 생성;
+	}
+
+private:
+	DamageUtils();
+	~DamageUtils();
+};
