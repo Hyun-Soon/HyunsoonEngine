8a73d56ab7396f2edd74844e9bba6eed19822006
Hyunsoon <66724166+Hyun-Soon@users.noreply.github.com>
2025-03-10 15:26:23 +0900

    feat: static Vector2::Left, Right, Up, Down

--- HyunsoonEngine_CommonSources/MathTypes.h
+++ HyunsoonEngine_CommonSources/MathTypes.h
@@ -8,6 +8,10 @@ namespace hs
 	{
 		static Vector2 Zero;
 		static Vector2 One;
+		static Vector2 Left;
+		static Vector2 Right;
+		static Vector2 Up;
+		static Vector2 Down;
 
 		float x;
 		float y;
